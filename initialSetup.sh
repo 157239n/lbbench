b95ce2954b832fc38f022b602282e69797d9e98f
Quang Ho <157239q@gmail.com>
2022-06-18 02:04:36 -0400

    Install node

--- initialSetup.sh
+++ initialSetup.sh
@@ -2,6 +2,8 @@
 
 apt-get update
 apt-get install -y git vim curl nginx apache2-utils && curl vim.kelvinho.org | bash 
+curl -sL https://deb.nodesource.com/setup_18.x | bash -
+apt-get update && apt-get install nodejs -y
 curl -L get.docker.com | bash
 docker network create main
 
