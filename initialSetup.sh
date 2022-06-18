096aca3b381d8f128798901abff1b209fecd2a95
Quang Ho <157239q@gmail.com>
2022-06-18 02:01:08 -0400

    Added curl -L

--- initialSetup.sh
+++ initialSetup.sh
@@ -2,6 +2,6 @@
 
 apt-get update
 apt-get install -y git vim curl nginx apache2-utils && curl vim.kelvinho.org | bash 
-curl get.docker.com | bash
+curl -L get.docker.com | bash
 docker network create main
 
