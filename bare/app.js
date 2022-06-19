fd6ba85f1c3014097684b8807e40086c0c2c5f46
Quang Ho <157239q@gmail.com>
2022-06-18 23:16:37 -0400

    Added multiprocess mode to node

--- bare/app.js
+++ bare/app.js
@@ -1,20 +1,30 @@
 import express from "express";
+import os from "os";
+import cluster from "cluster";
 
-const app = express();
+/**
+ * Runs on 1 core
+ */
 
-app.all("/", (req, res) => {
-  res.send("Ok");
-});
+const setup = (port) => {
+  const app = express();
 
-console.log(process.argv);
+  app.all("/", (req, res) => {
+    res.send("Ok");
+  });
 
-let port;
-try {
-  port = parseInt(process.argv[2]);
-} catch (e) {
-  port = 8000;
-}
-
-(async () => {
   app.listen(port, () => console.log("App started!"));
-})();
+};
+
+if (process.argv.length > 2) {
+  console.log("Single process");
+  setup(parseInt(process.argv[2]));
+} else {
+  if (cluster.isMaster) {
+    console.log(`Multi process: ${os.cpus().length}`);
+    for (let i = 0; i < os.cpus().length; i++) cluster.fork();
+    cluster.on("exit", (worker) => {
+      console.log(`Worker ${worker.id} has exited`);
+    });
+  } else setup(8000);
+}
--- initialSetup.sh
+++ initialSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y git vim curl apache2-utils
+apt-get install -y git vim curl apache2-utils parallel
 curl vim.kelvinho.org | bash 
 curl -sL https://deb.nodesource.com/setup_18.x | bash -
 apt-get update && apt-get install nodejs -y
